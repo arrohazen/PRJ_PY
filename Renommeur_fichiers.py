a78ae67e86000150b8ee507d9ddd9df7f00b8f5d
arrohazen <arrohazen@protonmail.com>
2021-11-23 10:44:19 +0100

    premier push test

---
+++ Renommeur_fichiers.py
@@ -0,0 +1,12 @@
+import os
+
+print("Vous êtes situé ici :", os.getcwd())
+
+emplacement = os.getcwd()
+
+fichiers = os.listdir(emplacement)
+
+print("Votre répertoire contient :", fichiers)
+
+for fichier in fichiers:
+    os.rename(fichier, fichier.replace(" ", "-").lower())
