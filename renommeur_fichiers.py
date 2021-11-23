e495b075809a8a05f8e9310536380c0243e389a4
arrohazen <arrohazen@protonmail.com>
2021-11-23 11:53:52 +0100

    Implémentation du script interactif

--- renommeur_fichiers.py
+++ renommeur_fichiers.py
@@ -2,12 +2,38 @@ import os
 
 print("Vous êtes situé ici :", os.getcwd())
 
-emplacement = os.getcwd()
+print("Souhaitez-vous vous déplacer dans un autre répertoire? Oui/Non : ")
+Quest1 = input()
 
-fichiers = os.listdir(emplacement)
+if Quest1 == "Oui":
+    print("Spécifiez le repertoire voulu (ex : /home/user/dossier) : ")
+    Quest2 = input()
 
-print("Votre répertoire contient :", fichiers)
+        os.walk(Quest2)
+        
+        emplacement = os.getcwd()
+        fichiers = os.listdir(emplacement)
 
-for fichier in fichiers:
-    os.rename(fichier, fichier.replace(" ", "-").lower())
+        print("Votre répertoire contient :", fichiers)
+
+            for fichier in fichiers:
+                 os.rename(fichier, fichier.replace(" ", "-").lower())
+
+elif Quest1 == "Non":
+    os.walk("/home/arrohazen/Documents/cy/fichierpy")
+    emplacement = os.getcwd()
+    
+    print("Le repertoire par défaut sera choisi : ", emplacement)
+
+    fichiers = os.listdir(emplacement)
+
+    print("Votre répertoire contient :", fichiers)
+
+        for fichier in fichiers:
+            os.rename(fichier, fichier.replace(" ", "-").lower())
+
+else :
+    print("Choisissez Oui ou Non!")
+
+
 #PREMIERE mODIF avec vs
