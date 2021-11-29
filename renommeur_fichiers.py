258c15670cabb568caba132203b165830e0f6323
arrohazen <arrohazen@protonmail.com>
2021-11-29 16:29:59 +0100

    def demander_repertoire

--- renommeur_fichiers.py
+++ renommeur_fichiers.py
@@ -5,14 +5,18 @@ print("Vous êtes situé ici :", os.getcwd())
 print("Souhaitez-vous vous déplacer dans un autre répertoire? Oui/Non : ")
 rep_quest = input()
 
+def demander_repertoire():
+    rep_quest = str(input("Spécifiez le repertoire voulu (ex : /home/user/dossier) : "))
+    os.chdir(rep_quest)
+    localisation = os.getcwd()
+
 def renommage() :
     for fichier in fichiers:
      os.rename(fichier, fichier.replace(" ", "-").lower())
 
 if rep_quest == "Oui":
-    rep_quest = str(input("Spécifiez le repertoire voulu (ex : /home/user/dossier) : "))
-    os.chdir(rep_quest)
-    emplacement = os.getcwd()
+
+    emplacement = demander_repertoire()
     fichiers = os.listdir(emplacement)
    
     print("Votre répertoire contient :", fichiers)
