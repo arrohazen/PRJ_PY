9ce90c7042f1e07a1c22460b6b2b373b1723157a
arrohazen <arrohazen@protonmail.com>
2022-01-21 16:11:27 +0100

    Adding comments

--- renommeur_fichiers.py
+++ renommeur_fichiers.py
@@ -3,38 +3,31 @@ import shutil
 import getpass
 import argparse
 
+
+#Usage of argparse to take usage method 
 parser = argparse.ArgumentParser(description= 'Choose a function (sorter, renamer, )')
 parser.add_argument( '-s', '--sorter',  action='store_true', help='Sorter of files by extension')
 parser.add_argument( '-r', '--rename',  action='store_true', help='Renaming files : replace spaces to -')
 parser.add_argument( '-l', '--location',  type=str, help='Giving a directory location')
-
 args = parser.parse_args()
 
-print("Bienvenue dans le script de renamer et de tri de fil! ")
-print("Vous êtes situé ici :", os.getcwd())
-
-#print("Souhaitez-vous vous déplacer dans un autre répertoire? Oui/Non : ")
-#rep_quest = input()
+print("Bienvenue dans le script de renamer et de tri de fichiers! ")
+#Get the user to make 
 user = getpass.getuser()
-
-def ask_dir():
-    rep_quest = str(input("Spécifiez le directory voulu (par défaut : /home/user/Téléchargements/) : "))
-    os.chdir(rep_quest)
-    localisation = os.getcwd()
-
+#Function rename : replacing spaces to -
 def renamer(files) :
     os.chdir(location)
     for file in files:
         os.rename(file, file.replace(" ", "-").lower())
-
+#Function who sort files 
 def trier_files():
     location_dest = "/home/"+user+"/"
     directory_names = ['Images', 'archives', 'Musique' , 'Paquets_Debian']
-    
+    #Create directories if they are not in the user directory
     for directory in directory_names:
         if not os.path.exists(location_dest+directory):
          os.makedirs(location_dest+directory)
-
+    #Organisate files by extension
     for file in files:
         if ".jpg" in file and not os.path.exists(location_dest+'Images/'+file):
             shutil.move(location+"/"+file, location_dest+'Images/'+file)
@@ -48,7 +41,7 @@ def trier_files():
             shutil.move(location+"/"+file, location_dest+'Musique/'+file)
         if ".deb" in file and not os.path.exists(location_dest+'Paquets_Debian/'+file):
             shutil.move(location+"/"+file, location_dest+'Paquets_Debian/'+file)
-
+#Rename condition
 if args.rename:
 
     location = args.location
@@ -61,6 +54,7 @@ if args.rename:
     
     print("Vos files ont été renommés : ", os.listdir(location)) 
 
+#Sort and location specification condition
 elif args.sorter and args.location:
 
     location = args.location
@@ -71,16 +65,12 @@ elif args.sorter and args.location:
     files = os.listdir(location)
 
     print("Votre répertoire contient :", files)
-        
-    #renamer(files)
-
-    #print("Vos files ont été renommés : ", os.listdir(location)) 
 
     trier_files()
 
     print("Vos files ont été triés : Musique, archives, Images... vers votre dossier personnel")
 
-
+#Sort with default location condition
 elif args.sorter and not args.location:
 
     os.chdir("/home/"+user+"/Téléchargements/")
@@ -91,15 +81,12 @@ elif args.sorter and not args.location:
     files = os.listdir(location)
 
     print("Votre répertoire contient :", files)
-        
-    #renamer(files)
-
-    #print("Vos files ont été renommés : ", os.listdir(location)) 
 
     trier_files()
 
     print("Vos files ont été triés : Musique, archives, Images... vers votre dossier personnel")
 
+#Rename condition with default location
 elif args.rename and not args.location:
     
     location = "/home/"+user+"/Téléchargements/"
